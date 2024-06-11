4101941ce83f94b6046ec3dfa54873e9b6e05a7b
michael <zdong2010@gmail.com>
2024-06-11 23:24:07 +0800

    feat: 添加interceptor拦截器-全局/provide/handler

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Request, Response, NextFunction } from 'express';
 import { LoginGuard } from './login.guard';
+import { TimeInterceptor } from './time.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   /**
@@ -16,6 +17,8 @@ async function bootstrap() {
    * 全局守卫
    */
   //app.useGlobalGuards(new LoginGuard());
+  // 作用与所有Controller的拦截器
+  //app.useGlobalInterceptors(new TimeInterceptor());
   await app.listen(3000);
 }
 bootstrap();
