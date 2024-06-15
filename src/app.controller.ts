dc2cfb45817963afe3cf897c53ade4e916163c98
michael <zdong2010@gmail.com>
2024-06-15 13:21:34 +0800

    feat: 自定义query装饰器

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, SetMetadata, UseGuards, Headers } from '@nestjs/common';
+import { Controller, Get, SetMetadata, UseGuards, Headers, Query, ParseIntPipe } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AaaGuard } from './aaa.guard';
 import { Aaa } from './aaa.decorator';
 import { Bbb } from './bbb.decorator';
 import { MyHeaders } from './myheaders.decorator';
+import { MyQuery } from './my-query.decorator';
 
 @Controller()
 export class AppController {
@@ -32,5 +33,12 @@ export class AppController {
   getHello5(@Headers('Accept') header1: string, @MyHeaders('Accept') header2: string) {
     console.log('header1', header1)
     console.log('header2', header2)
+  }
+
+  @Get('hello6')
+  getHello6(@Query('aaa', new ParseIntPipe()) aaa: string,@MyQuery('bbb', new ParseIntPipe()) bbb:string) {
+    console.log('aaa', aaa+1)
+    console.log('bbb', bbb + 2)
+    return 'custom query decorator'
   }
 }
