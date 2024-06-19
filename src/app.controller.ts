bdda19ea7ecef328adc31f7060b31a5a2d6c9fa4
michael <zdong2010@gmail.com>
2024-06-19 11:31:11 +0800

    feat: 切换到fastify请求库

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, Response } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FastifyRequest, FastifyReply } from 'fastify';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // @Response({ passThrough: true}) reply: FastifyReply
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(
+    @Request() request: FastifyRequest,
+    @Response() reply: FastifyReply,
+  ) {
+    reply.header('url', request.url);
+    reply.send('Hello World!!');
+    //return this.appService.getHello();
   }
 }
