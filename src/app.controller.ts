b7286180c78b6278bf1c07665539830389ecf1e8
michael <zdong2010@gmail.com>
2024-06-21 16:21:47 +0800

    feat: 添加version版本号(HEADER/MEDIA_TYPE/URI/CUSTOM)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, VERSION_NEUTRAL, Version } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller({
+  path: 'api', // 设置路由前缀为 /api
+  //version: VERSION_NEUTRAL, // 设置版本号为 1
+  version: ['1', '3'],
+})
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  @Version('2')
+  @Get()
+  findAllV2() {
+    return this.appService.getHello() + ' V2';
+  }
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+import { Request } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // header中携带version信息
+  /*
+  app.enableVersioning({
+    type: VersioningType.HEADER,
+    header: 'version',
+  });
+  */
+  // Accept头信息逗号分隔vv=1
+  /*
+  app.enableVersioning({
+    type: VersioningType.MEDIA_TYPE,
+    key: 'vv=',
+  });
+  */
+  //url地址v1前缀
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  // 自定义版本提取器
+  /*
+  const extractor = (request: Request) => {
+    if (request.headers['disable-custom']) {
+      return '';
+    }
+    return request.url.includes('v') ? '2' : '1';
+  };
+  app.enableVersioning({
+    type: VersioningType.CUSTOM,
+    extractor,
+  });
+  */
   await app.listen(3000);
 }
 bootstrap();
