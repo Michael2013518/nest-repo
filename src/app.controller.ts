16e9de4f2935204104e67a027a36fb8a33dfcb1a
michael <zdong2010@gmail.com>
2024-06-25 16:34:01 +0800

    feat: 大文件分片上传

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UseInterceptors,
+  UploadedFiles,
+  Body,
+  Query,
+} from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { FilesInterceptor } from '@nestjs/platform-express';
+import * as fs from 'fs';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +17,65 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    FilesInterceptor('files', 20, {
+      dest: 'uploads',
+    }),
+  )
+  uploadFiles(
+    @UploadedFiles() files: Array<Express.Multer.File>,
+    @Body() body,
+  ) {
+    console.log('body', body);
+    console.log('files', files);
+    const fileName = body.name.match(/(.+)\-\d+$/)[1];
+    const chunkDir = 'uploads/chunks_' + fileName;
+    // 合并分片
+    if (!fs.existsSync(chunkDir)) {
+      fs.mkdirSync(chunkDir);
+    }
+    fs.cpSync(files[0].path, chunkDir + '/' + body.name);
+    fs.rmSync(files[0].path);
+    //return 'success';
+  }
+
+  @Get('merge')
+  merge(@Query('name') name: string) {
+    const chunkDir = 'uploads/chunks_' + name;
+
+    const files = fs.readdirSync(chunkDir);
+
+    let count = 0;
+    let startPos = 0;
+    files.map((file) => {
+      const filePath = chunkDir + '/' + file;
+      const stream = fs.createReadStream(filePath);
+      stream
+        .pipe(
+          fs.createWriteStream('uploads/' + name, {
+            start: startPos,
+          }),
+        )
+        .on('finish', () => {
+          count++;
+
+          if (count === files.length) {
+            fs.rm(
+              chunkDir,
+              {
+                recursive: true,
+              },
+              () => {
+                console.log('删除成功');
+              },
+            );
+          }
+        });
+
+      startPos += fs.statSync(filePath).size;
+    });
   }
 }
