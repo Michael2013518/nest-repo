2cf771ef16b71a7a8f1d142f0a51478ec1854fc4
michael <zdong2010@gmail.com>
2024-08-14 15:57:20 +0800

    feat: 添加swagger文档

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse, ApiQuery } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,18 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('test')
+  @ApiOperation({ summary: '测试接口', description: '用于应用接口测试' })
+  @ApiQuery({
+    name: 'Params',
+    type: String,
+    description: '参数一',
+    required: false,
+    example: 'test',
+  })
+  @ApiResponse({ status: HttpStatus.OK, description: '测试成功', type: String })
+  appTest() {
+    return 'Test Api';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // swagger文档配置
+  const config = new DocumentBuilder()
+    .setTitle('应用接口文档')
+    .setDescription(
+      '应用接口文档包括用户、角色、权限、菜单、字典、日志、文件等接口',
+    )
+    .setVersion('1.0')
+    .addTag('测试接口')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
